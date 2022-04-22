a5a5bae72646a35045e94e3ae02a44295b750fe9
Mikatzu <mr.frienk@gmail.com>
2022-04-22 18:13:43 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!--  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">  -->
+    <link rel="stylesheet" href="css/app.css">
+    <title>Hori-Hori</title>
+</head>
+<body>
+
+    <div class="navbar">
+        <a href="" class="hori-sign"><img src="/image/Hori_sign.svg" alt=""></a>
+        <div class="rectangle3"><a href="" class="catalog-button">КАТАЛОГ</a></div>
+        <div class ='rectangle1'><input class="input-navbar" type="text" placeholder=""></div>
+        <div class="search-button"><a href="" class="search-pic"><img src="/image/Search_button.svg" alt=""></a></div>
+        <a href=""><div class="pfp"></div></a>
+    </div>
+    <div class="div1">
+        <div class="bigtext"><img src="/image/Hori_bigtext.svg" alt=""></div>
+        <div class="bestsellers"><p>Бестселлеры</p></div>
+        <a href=""><div class="bestseller1"><div class="regtangle_bestseller1"><p class="bestseller1-t">ELDEN RING Deluxe edition 50$</p><img src="" alt=""></div></div></a>
+        <a href=""><div class="bestseller2"><div class="regtangle_bestseller2"><p class="bestseller2-t">NieR:Automata 20$</p><img src="" alt=""></div></div></a>
+        <a href=""> <div class="bestseller3"><div class="regtangle_bestseller3"><p class="bestseller3-t">Battlefield 49.99$</p><img src="" alt=""></div></div></a>
+    </div>
+    <div class="div2">
+        <div class="bestcosts"><img src="/image/bestcosts.svg" alt=""></div>
+        <div class="krugsupport"><img src="/image/krugsupport.svg" alt=""></div>
+        <div class="bestachievements"><img src="/image/bestachievements.svg" alt=""></div>
+        <div class="bestreviews"><img src="/image/bestreviews.svg" alt=""></div>
+    </div>
+    <div class="sell_case">
+        <a href="">НОВИНКИ</a>
+        <a href="">ЛИДЕРЫ ПРОДАЖ</a>
+        <a href="">ПРЕДЗАКАЗЫ</a>
+    </div>
+</body>
+</html>
