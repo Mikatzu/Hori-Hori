65cd79d2a1010a24c1698bc1f03ecabbc02ca6c5
Mikatzu <mr.frienk@gmail.com>
2022-04-24 00:04:44 +0600

    3.0v

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -10,31 +10,50 @@
     <title>Hori-Hori</title>
 </head>
 <body>
-
+    <div class="navbargroup">
     <div class="navbar">
         <a href="" class="hori-sign"><img src="/image/Hori_sign.svg" alt=""></a>
-        <div class="rectangle3"><a href="" class="catalog-button">КАТАЛОГ</a></div>
+        <a href="" class="catalog-button"><div class="rectangle3">КАТАЛОГ</div></a>
         <div class ='rectangle1'><input class="input-navbar" type="text" placeholder=""></div>
         <div class="search-button"><a href="" class="search-pic"><img src="/image/Search_button.svg" alt=""></a></div>
         <a href=""><div class="pfp"></div></a>
     </div>
+    </div>
+
     <div class="div1">
         <div class="bigtext"><img src="/image/Hori_bigtext.svg" alt=""></div>
         <div class="bestsellers"><p>Бестселлеры</p></div>
-        <a href=""><div class="bestseller1"><div class="regtangle_bestseller1"><p class="bestseller1-t">ELDEN RING Deluxe edition 50$</p><img src="" alt=""></div></div></a>
-        <a href=""><div class="bestseller2"><div class="regtangle_bestseller2"><p class="bestseller2-t">NieR:Automata 20$</p><img src="" alt=""></div></div></a>
-        <a href=""> <div class="bestseller3"><div class="regtangle_bestseller3"><p class="bestseller3-t">Battlefield 49.99$</p><img src="" alt=""></div></div></a>
+        <a href=""><div class="bestseller1"><div class="regtangle_bestseller1"><p class="bestseller1-t">ELDEN RING Deluxe edition 50$</p></div></div><img src="/image/elden_ringdeluxe.jpg" alt="" class="bestsellerpic1"></a>
+        <a href=""><div class="bestseller2"><div class="regtangle_bestseller2"><p class="bestseller2-t">NieR:Automata 20$</p></div></div><img src="/image/nierautomata.jpg" alt="" class="bestsellerpic2"></a>
+        <a href=""> <div class="bestseller3"><div class="regtangle_bestseller3"><p class="bestseller3-t">Battlefield 49.99$</p></div></div><img src="/image/Battlefield2042.jpg" alt="" class="bestsellerpic3"></a>
     </div>
+
     <div class="div2">
         <div class="bestcosts"><img src="/image/bestcosts.svg" alt=""></div>
         <div class="krugsupport"><img src="/image/krugsupport.svg" alt=""></div>
         <div class="bestachievements"><img src="/image/bestachievements.svg" alt=""></div>
         <div class="bestreviews"><img src="/image/bestreviews.svg" alt=""></div>
     </div>
+
     <div class="sell_case">
-        <a href="">НОВИНКИ</a>
-        <a href="">ЛИДЕРЫ ПРОДАЖ</a>
-        <a href="">ПРЕДЗАКАЗЫ</a>
+        <div class="buttons_up">
+            <a href="" class="newprod"><div class="newprod_button">НОВИНКИ</div></a>
+            <a href="" class="leadsells"><div class="leadsells_button">ЛИДЕРЫ ПРОДАЖ</div></a>
+            <a href="" class="preorders"><div class="preorders_button">ПРЕДЗАКАЗЫ</div></a>
+        </div>
+        <div class="dynamic_products">
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+            <a href="" class="dynamic_product"><div class="dynamic_product_rectangle"><img src="" alt="">Lorem ipsum dolor sit.</div></a>
+        </div>
     </div>
+
 </body>
 </html>
