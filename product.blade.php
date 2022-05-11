f92db006b72e416cb24c4ac8fef4ebce66a19eee
Mikatzu <85638740+Mikatzu@users.noreply.github.com>
2022-05-11 20:22:54 +0600

    upload manualy

---
+++ product.blade.php
@@ -0,0 +1,75 @@
+@extends('layout')
+@section('title')Продукт@endsection
+@section('main_content')
+    <div class="">
+
+    </div>
+    <div class="standart_goty_buttons">
+
+        <div class="standart_button">
+            <a href="" class="standart_product_button">
+                <a href="" class="standart_product_button_text">
+                    Standart
+                </a>
+            </a>
+        </div>
+
+    <div class="deluxe_button">
+        <a href="" class="deluxe_product_button">
+            <a href="" class="deluxe_product_button_text">
+                Deluxe
+            </a>
+        </a>
+    </div>
+
+    <div class="goty_button">
+        <a href="" class="goty_product_button">
+            <a href="" class="goty_product_button_text">
+                Game of the Year
+            </a>
+        </a>
+    </div>
+
+    </div>
+
+    <div class="big_product_pic">
+        <div class="product_pic_rectangle">
+        </div>
+    </div>
+
+    <img src="/image/nierautomata.jpg" alt="" class="real_product_pic">
+
+    <div class="name_and_cost">
+        <h1 class="name">NieR: Automata</h1>
+        <p class="lang">Языки: Русский, Английский, Японский</p>
+    </div>
+    <h1 class="screenshot_text">Скриншоты</h1>
+    <div class="product_screenshots">
+            <div class="screenshot">1<img src="" alt="" class="screenshot_img"></div>
+            <div class="screenshot">2<img src="" alt="" class="screenshot_img"></div>
+            <div class="screenshot">3<img src="" alt="" class="screenshot_img"></div>
+            <div class="screenshot">4<img src="" alt="" class="screenshot_img"></div>
+            <div class="screenshot">5<img src="" alt="" class="screenshot_img"></div>
+            <div class="screenshot">6<img src="" alt="" class="screenshot_img"></div>
+    </div>
+
+    <div class="genres">
+        <div class="genre"><p>Жанр</p><p>Экшн, Приключения, Драма</p></div>
+        <div class="genre"><p>Платформа</p><p>PC, XBOX, PS4</p></div>
+        <div class="genre"><p>Издатель</p><p>Platinum Games</p></div>
+        <div class="genre"><p>Разработчик</p><p>Platinum Games</p></div>
+    </div>
+
+    <div class="review_requirements">
+
+    </div>
+
+    <div class="similargames">
+
+    </div>
+
+    <div class="commentary">
+
+    </div>
+
+@endsection
